3499f1d795dd33d16a6c14f5e9c48be76ab501f8
KubiiCz <jakub.73@hotmail.com>
2021-07-08 08:30:20 +0200

    added alt to image

--- components/CrewMemebrCard/CrewMemberCard.js
+++ components/CrewMemebrCard/CrewMemberCard.js
@@ -3,7 +3,8 @@ import BtnContainer from '../BtnContainer/BtnContainer'
 const CrewMemberCard = ({ astronaut, preview }) => {
   return (
     <div className="card">
-      <img src={astronaut.image_url}></img>
+
+      <img src={astronaut.image_url} alt="Astronaut profile image"></img>
       <h1 className="astronautName">{astronaut.name}</h1>
       <h2 className="astronautSurname">{astronaut.surname}</h2>
       <span>Was born in: {astronaut.birth_date}</span>
